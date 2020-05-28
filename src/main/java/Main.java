c19ade46db460d10cb6bf472e2f3deb25c98dc8d
bngams <n.boris@devidia.net>
2020-05-28 13:58:07 +0200

    first commit, read file using Files API

---
+++ src/main/java/Main.java
@@ -0,0 +1,38 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(String[] args) {
+        readLines();
+        readLinesWithFilter();
+    }
+
+    public static void readLines() {
+        System.out.println("===== Read lines");
+        String fileName = "days.txt";
+        try (Stream stream = Files.lines(Paths.get(fileName))) {
+            stream.forEach(System.out::println);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void readLinesWithFilter() {
+        System.out.println("===== Read lines and filter");
+        String fileName = "days.txt";
+        try {
+            Stream<String> lines = Files.lines(Paths.get(fileName));
+            Optional hasEdi = lines.filter(s -> s.contains("edi")).findFirst();
+            if(hasEdi.isPresent()){
+                System.out.println(hasEdi.get());
+            }
+            //Fermer le flux
+            lines.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
